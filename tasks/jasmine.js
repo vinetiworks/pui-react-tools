33d4b7d9a5d23370f07c90e563103b68a5b5baa2
Midhun Krishna <midhun.krishna@vineti.com>
2020-04-21 15:40:45 +0530

    test-dist

--- tasks/jasmine.js
+++
@@ -1,7 +0,0 @@
-const gulp = require('gulp');
-const {jasmine} = require('gulp-load-plugins')();
-
-gulp.task('spec', () => {
-  return gulp.src('spec/**/*_spec.js')
-    .pipe(jasmine({includeStackTrace: true}));
-});
